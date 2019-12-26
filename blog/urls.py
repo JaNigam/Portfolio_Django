78c4e761c9c0e5b8739bbdaff4017eb7e90d4cdc
Ritvik Dayal <ritvikr1605@gmail.com>
2019-12-26 14:31:31 +0530

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('blog/', views.blog, name='blog-blog'),
     path('contact/', views.contact, name='blog-contact'),
-    path('work/', views.work, name='blog-work'),
-    path('about/', views.blog_about, name='blog-about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-	{
-	 'author': 'Ritvik Rathore',
-	 'title': 'Blog Post 1',
-	 'content': 'First post content',
-	 'date_posted': 'Augut 27, 2018'
-	},
-	{
-	 'author': 'Ritvik Dayal',
-	 'title': 'Blog Post 2',
-	 'content': 'second post content',
-	 'date_posted': 'Augut 28, 2018'
-	}
-]
-
 def home(request):
 	return render(request, 'blog/index.html')
 
-def blog_about(requests):
-	return render(requests, 'blog/blog_about.html')
-
 def blog(request):
-	context = {
-	'posts': posts
-	}
-	return render(request, 'blog/blog.html', context)
+	return render(request, 'blog/blog.html')
 
 def contact(request):
 	return render(request, 'blog/contact.html')
-
-def work(requests):
-	return render(request, 'blog/work.html')
