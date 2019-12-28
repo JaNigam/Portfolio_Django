a75ce081f54d3df4e8f96af96127860b87a4daef
Ritvik Dayal <ritvikr1605@gmail.com>
2019-12-28 21:41:57 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-from . import views
+def home(request):
+	return render(request, 'blog/index.html')
 
-urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('blog/', views.blog, name='blog-blog'),
-    path('contact/', views.contact, name='blog-contact'),
-]
+def blog(request):
+	context = {
+	'title': 'Blog',
+	'posts': Post.objects.all()
+	}
+	return render(request, 'blog/blog.html', context)
+
+def contact(request):
+	return render(request, 'blog/contact.html', {'title': 'Contact'})
