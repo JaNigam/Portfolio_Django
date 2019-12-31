4864d23ed170a56834ac73a4779eccdb9ccf705e
Ritvik Dayal <ritvikr1605@gmail.com>
2019-12-31 22:40:53 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def home(request):
 	return render(request, 'blog/index.html')
 
 def blog(request):
-	return render(request, 'blog/blog.html')
+	context = {
+	'title': 'Blog',
+	'posts': Post.objects.all()
+	}
+	return render(request, 'blog/blog.html', context)
 
 def contact(request):
-	return render(request, 'blog/contact.html')
+	return render(request, 'blog/contact.html', {'title': 'Contact'})
